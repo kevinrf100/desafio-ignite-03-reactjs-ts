98b5a37531f90b08b050af57cdf08742697030e4
Kevin Rossetti Fernandes <kevinrf100@gmail.com>
2024-02-26 21:44:23 -0300

    Adding code style according to Markdown documentation

--- src/pages/Post/components/PostDetails/index.tsx
+++ src/pages/Post/components/PostDetails/index.tsx
@@ -1,14 +1,39 @@
 import Markdown from "react-markdown";
 import { PostContentContainer } from "./styles";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Components } from "hast-util-to-jsx-runtime";
 
 interface PostContentProps {
   content: string;
 }
 
 export function PostContent({ content }: PostContentProps) {
+  const MarkdownComponents: Partial<Components> = {
+    code(props) {
+      const { children, className, ...rest } = props;
+      const match = /language-(\w+)/.exec(className || "");
+      return match ? (
+        <SyntaxHighlighter
+          {...rest}
+          PreTag="div"
+          language={match[1]}
+          style={dracula}
+          ref={null}
+        >
+          {String(children).replace(/\n$/, "")}
+        </SyntaxHighlighter>
+      ) : (
+        <code {...rest} className={className}>
+          {children}
+        </code>
+      );
+    },
+  };
+
   return (
     <PostContentContainer>
-      <Markdown>{content}</Markdown>
+      <Markdown components={MarkdownComponents}>{content}</Markdown>
     </PostContentContainer>
   );
 }
